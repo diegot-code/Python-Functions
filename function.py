228667b85e65805da140c7e9197e1852d2f48787
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-02-23 23:08:00 -0500

    second commit
    Added 2-3 more functions and clarified local variables.

--- function.py
+++ function.py
@@ -15,7 +15,7 @@ def givex():
 
 # givex()
 
-def displaying_firstname(fname): # 'fname' is a parameter of the function
+def displaying_firstname(fname): # 'fname' is a parameter of the func
     print(f'{fname} has entered the chatroom.')
 
 # displaying_firstname("Ezekiel")
@@ -32,4 +32,11 @@ def display_name_age(fname, age):
 
 display_name_age("Ava", 31)
 
+def calculate_square_area(side_length):
+    area = side_length ** 2
+    print(f'The area of the square with side length {side_length} is {area} square units.')
+
+# calculate_square_area(5)
+# calculate_square_area(8)
+# calculate_square_area(10)
 
